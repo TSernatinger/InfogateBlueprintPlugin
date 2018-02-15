a70892d16baecb5da1018214f81e3a45004502ac
TSernatinger <tobias.sernatinger@infogate.ch>
2018-02-15 17:45:43 +0100

    Removed sample template.
    - Deleted files for the sample template.

--- src/main/resources/js/simplebp.js
+++
@@ -1,9 +0,0 @@
-Confluence.Blueprint.setWizard('infogate.plugins.confluence.blueprints.ig.blueprintplugin:create-by-sample-template', function(wizard) {
-	wizard.on('submit.page1Id', function(e, state) {   
-        var vName = state.pageData.vName;
-        if (!vName) {
-            alert('Please provide a name value.');
-            return false;
-        }
-    });
-});
